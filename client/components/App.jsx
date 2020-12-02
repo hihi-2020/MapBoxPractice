6d84cf329d045c73b4a3065d1316e143e60f7908
mackenadam <mackenadam@gmail.com>
2020-12-02 20:09:41 +1300

    Working mapbox map with fixed starting co-ords, and state that updates in relation to the center of the map view

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,22 +2,14 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import { fetchProducts } from '../actions'
-import Cart from './Cart'
-import Orders from './Orders'
-import Products from './Products'
+import Map from './Map'
 
 export class App extends React.Component {
-  componentDidMount () {
-    this.props.dispatch(fetchProducts())
-  }
 
   render () {
     return (
       <div className='app'>
-        <h1>Let's Go Shopping!</h1>
-        <Products/>
-        <Cart/>
-        <Orders/>
+        <Map />
       </div>
     )
   }
---
+++ client/components/Map.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import mapboxgl from 'mapbox-gl'
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFja2VuYWRhbSIsImEiOiJja2k3MHE1aDEwcmF2MnJvbGd2NWE5aW9mIn0.fFQVww5WDzwFB5zgovZ6NQ'
+
+class Map extends React.Component {
+
+    state = {
+        start: {
+            lng: 5,
+            lat: 34,
+            zoom: 2
+        }
+    }
+
+    componentDidMount () {
+        const map = new mapboxgl.Map({
+            container: this.mapContainer,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [this.state.start.lng, this.state.start.lat],
+            zoom: this.state.start.zoom
+        })
+        map.on('move', () => {
+            this.setState({
+                start: {
+                    lng: map.getCenter().lng.toFixed(4),
+                    lat: map.getCenter().lat.toFixed(4),
+                    zoom: map.getZoom(). toFixed(2)
+                }
+            })
+        })
+    }
+
+    render () {
+        return (
+        <div>
+            <div ref={el => this.mapContainer = el} className="mapContainer" />
+        </div>
+        )
+    }
+}
+
+export default Map
