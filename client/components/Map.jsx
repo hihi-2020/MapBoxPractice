a79dd624c1b24d70f04a2d53254725590f6b8e9a
mackenadam <mackenadam@gmail.com>
2020-12-02 22:10:45 +1300

    Add start and finish location markers which update state using onDragEnd method

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,43 +1,73 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
 import mapboxgl from 'mapbox-gl'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFja2VuYWRhbSIsImEiOiJja2k3MHE1aDEwcmF2MnJvbGd2NWE5aW9mIn0.fFQVww5WDzwFB5zgovZ6NQ'
 
 class Map extends React.Component {
-
     state = {
-        start: {
-            lng: 5,
-            lat: 34,
-            zoom: 2
-        }
+      initial: {
+        lng: 174.7571,
+        lat: -41.2873,
+        zoom: 10
+      },
+      start: {
+        lng: 174.7571,
+        lat: -41.2873,
+        zoom: 10
+      },
+      finish: {
+        lng: 174.7571,
+        lat: -41.2873,
+        zoom: 10
+      }
     }
 
     componentDidMount () {
-        const map = new mapboxgl.Map({
-            container: this.mapContainer,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [this.state.start.lng, this.state.start.lat],
-            zoom: this.state.start.zoom
+      const map = new mapboxgl.Map({
+        container: this.mapContainer,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [this.state.initial.lng, this.state.initial.lat],
+        zoom: this.state.initial.zoom
+      })
+
+      const onDragEnd = (marker, stateKey) => {
+        const lngLat = marker.getLngLat()
+        console.log(lngLat)
+
+        this.setState({
+          [stateKey]: {
+            lng: lngLat.lng,
+            lat: lngLat.lat,
+            zoom: 10
+          }
         })
-        map.on('move', () => {
-            this.setState({
-                start: {
-                    lng: map.getCenter().lng.toFixed(4),
-                    lat: map.getCenter().lat.toFixed(4),
-                    zoom: map.getZoom(). toFixed(2)
-                }
-            })
-        })
+      }
+
+      const startMarker = new mapboxgl.Marker({
+        draggable: true,
+        color: '#00ff00'
+      })
+        .setLngLat([174.7571, -41.2873])
+        .addTo(map)
+
+      const finishMarker = new mapboxgl.Marker({
+        draggable: true,
+        color: '#ff3300'
+      })
+        .setLngLat([174.7571, -41.2873])
+        .addTo(map)
+
+      startMarker.on('dragend', () => { onDragEnd(startMarker, 'start') })
+      finishMarker.on('dragend', () => { onDragEnd(finishMarker, 'finish') })
     }
 
     render () {
-        return (
+      return (
         <div className='mapFrame'>
-            <div ref={el => this.mapContainer = el} className="mapContainer" />
+          <div ref={(el) => { this.mapContainer = el }} className="mapContainer" />
         </div>
-        )
+      )
     }
 }
 
